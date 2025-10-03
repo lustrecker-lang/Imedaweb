29d991619bacc4bcf316abfc49699a00b3166594
Luiz Str <lu.strecker@gmail.com>
2025-10-03 15:33:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4737506416-70449",
+  "appId": "1:1008843001771:web:32fd3040825ffc15fd0cee",
+  "apiKey": "AIzaSyB8tV70Hw5HjL0r6lQae-qkMIsYOIN-qEA",
+  "authDomain": "studio-4737506416-70449.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1008843001771"
+};
