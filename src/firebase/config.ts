e039e43d6aec217d66202cb1fc60d0f7302ad5d4
Luiz Str <lu.strecker@gmail.com>
2025-10-03 17:12:35 +0000

    STOP IT!!!!!!!! leave the config how it was!!! It's // For Firebase JS S

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyCU8-tl1Y7jODzvEjGcrXlscVrbZCfWwCs",
   "authDomain": "imedawebsite-98ced.firebaseapp.com",
   "projectId": "imedawebsite-98ced",
-  "storageBucket": "imedawebsite-98ced.appspot.com",
+  "storageBucket": "imedawebsite-98ced.firebasestorage.app",
   "messagingSenderId": "31527825121",
   "appId": "1:31527825121:web:edeb6d5af2eafb885bd1a0",
   "measurementId": "G-0HZRN72X6H"
