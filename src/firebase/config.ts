e0dd3e51d5a46135955d8573107d35a038574d01
Luiz Str <lu.strecker@gmail.com>
2025-10-03 16:03:03 +0000

    Your project Project name imedawebsite Project ID imedawebsite-98ced Pro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,5 @@
 export const firebaseConfig = {
-  "projectId": "studio-4737506416-70449",
-  "appId": "1:1008843001771:web:32fd3040825ffc15fd0cee",
-  "apiKey": "AIzaSyB8tV70Hw5HjL0r6lQae-qkMIsYOIN-qEA",
-  "authDomain": "studio-4737506416-70449.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1008843001771"
+  "projectId": "imedawebsite-98ced",
+  "apiKey": "AIzaSyCU8-tl1Y7jODzvEjGcrXlscVrbZCfWwCs",
+  "authDomain": "imedawebsite-98ced.firebaseapp.com"
 };
