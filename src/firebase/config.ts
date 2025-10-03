1f8ed9f5d72ae454069c2a412ca434350bab886e
Luiz Str <lu.strecker@gmail.com>
2025-10-03 16:53:47 +0000

    When I upload a file I get
    Runtime Error
    
    FirebaseError: Firebase Stora

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 export const firebaseConfig = {
   "projectId": "imedawebsite-98ced",
   "apiKey": "AIzaSyCU8-tl1Y7jODzvEjGcrXlscVrbZCfWwCs",
-  "authDomain": "imedawebsite-98ced.firebaseapp.com"
+  "authDomain": "imedawebsite-98ced.firebaseapp.com",
+  "storageBucket": "imedawebsite-98ced.appspot.com"
 };
