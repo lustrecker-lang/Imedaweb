7e9f5a42bf95a35d51ae9018cba84dbc073ebdcf
Luiz Str <lu.strecker@gmail.com>
2025-10-03 17:07:53 +0000

    Please make sure to fix the config file properly:
    // For Firebase JS SD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "imedawebsite-98ced",
   "apiKey": "AIzaSyCU8-tl1Y7jODzvEjGcrXlscVrbZCfWwCs",
   "authDomain": "imedawebsite-98ced.firebaseapp.com",
-  "storageBucket": "imedawebsite-98ced.appspot.com"
+  "projectId": "imedawebsite-98ced",
+  "storageBucket": "imedawebsite-98ced.appspot.com",
+  "messagingSenderId": "31527825121",
+  "appId": "1:31527825121:web:edeb6d5af2eafb885bd1a0",
+  "measurementId": "G-0HZRN72X6H"
 };
